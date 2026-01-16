d783f5712a7ff0f6d2f63498dc4260be8025637b
Jaka Krajnc <444.ous@gmail.com>
2026-01-16 15:11:55 +0100

    with air hotreload and source markdown

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func main() {
 		http.ServeFile(w, r, "serve/index.html")
 	})
 
+	http.HandleFunc("/source", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "serve/source.html")
+	})
+
 	log.Println("Server running at http://localhost:3232")
 	if err := http.ListenAndServe(":3232", nil); err != nil {
 		log.Fatal(err)
